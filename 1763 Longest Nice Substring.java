cd8461b6afa539f9b5a9f7f3fbe29bf0b8238cf0
Priyanka-N2781 <priyanka.n2781@gmail.com>
2026-04-27 12:22:04 +0530

    Implement longest nice substring function

---
+++ 1763 Longest Nice Substring.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        int n = s.length();
+        String ans = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                String sub = s.substring(i, j);
+                if (isNice(sub) && sub.length() > ans.length()) {
+                    ans = sub;
+                }
+            }
+        }
+        return ans;
+    }
+    private boolean isNice(String s) {
+    boolean[] lower = new boolean[26];
+    boolean[] upper = new boolean[26];
+
+    for (char c : s.toCharArray()) {
+        if (Character.isLowerCase(c)) {
+            lower[c - 'a'] = true;
+        } else {
+            upper[c - 'A'] = true;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if (lower[i] != upper[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+}
