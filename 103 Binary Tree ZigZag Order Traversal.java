cb4c5098d35d03ff7f3d283ea567661a48a134ec
Priyanka N <priyanka.n2781@gmail.com>
2026-05-08 11:22:32 +0530

    Implement zigzag level order traversal for binary tree

---
+++ 103 Binary Tree ZigZag Order Traversal.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
+    {
+        Queue<TreeNode> Q1 = new LinkedList<>();
+        List<List<Integer>> iRet = new ArrayList<>();
+        boolean iflag = false;
+
+        if(root == null) return iRet;
+
+        Q1.offer(root);
+
+        while(!Q1.isEmpty())
+        {
+            int size = Q1.size();
+            List<Integer> L1 = new ArrayList<>();
+            iflag = !iflag;
+
+            while(size > 0)
+            {
+                TreeNode temp = Q1.poll();
+                if(temp.left != null) Q1.offer(temp.left);
+
+                if(temp.right != null) Q1.offer(temp.right);
+
+                if(iflag == true)
+                {
+                    L1.add(temp.val);
+                }
+                else
+                {
+                    L1.add(0,temp.val);
+                }
+                size--;
+            }
+            iRet.add(L1);
+        }
+        return iRet;
+    }
+}
