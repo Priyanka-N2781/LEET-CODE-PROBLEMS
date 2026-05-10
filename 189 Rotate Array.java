27177870f5980184f94554a72bd2587b7bb340c6
Priyanka N <priyanka.n2781@gmail.com>
2026-05-10 09:58:29 +0530

    Add RotateArray class with rotation functionality
    Implement array rotation using reversal algorithm.

---
+++ 189 Rotate Array.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class RotateArray {
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public static void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        rotate(nums, k);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
