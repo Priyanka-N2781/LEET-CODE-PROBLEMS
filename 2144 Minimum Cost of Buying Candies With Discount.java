48cd8cce11f1a9a7f08dcb9882fae1695ca5e073
Priyanka N <priyanka.n2781@gmail.com>
2026-06-01 15:13:11 +0530

    Implement minimum cost calculation for candies

---
+++ 2144 Minimum Cost of Buying Candies With Discount.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int t = 0;
+        int l = cost.length;
+        for(int i=0;i<l;i+=3){
+            t += cost[l-1-i];
+            if(i+1 < l){
+                t += cost[l-1-(i+1)];
+            }
+        }
+        return t;
+    }
+}
